b54914088f8503c236a2c6486e61e351d31953c6
Liubov <kortcova@gmail.com>
2022-04-07 00:19:07 +0300

    JS first homework

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+let firstName = "Liubov";
+let last_name = "Kytsenko";
+let age = 29;
+let user = firstName + " " + last_name;
+console.log(user);
+
+// let FirstName;
+// let last name = "Kytsenko";
+// let age29 = 29;
