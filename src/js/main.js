fc9b9701f31356bb0f169e471fda57d6083a73e2
Liubov <kortcova@gmail.com>
2022-04-07 20:16:52 +0300

    js data type

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
-let firstName = "Liubov";
-let last_name = "Kytsenko";
-let age = 29;
-let user = firstName + " " + last_name;
-console.log(user);
-
-// let FirstName;
-// let last name = "Kytsenko";
-// let age29 = 29;
+let boolean = "true/false";
+let string = "string";
+let bigInt = 55555558768768795n;
+let obj = {
+  age: 23,
+  name: "Liuba",
+  lastName: "Kytsenko",
+};
+obj.name = prompt("Vvedi name");
+obj.lastName = prompt("Vvedi name");
+console.log("Hello, ", obj.name, obj.lastName);
