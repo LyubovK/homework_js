472cda2c5a2246d8f99d57d92676500880fdea62
Liubov <kortcova@gmail.com>
2022-04-08 23:00:55 +0300

    js object

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,28 @@
-let boolean = "true/false";
-let string = "string";
-let bigInt = 55555558768768795n;
-let obj = {
-  age: 23,
+let me = {
+  age: 29,
   name: "Liuba",
   lastName: "Kytsenko",
+  kids: 2,
+  work: true,
 };
-obj.name = prompt("Vvedi name");
-obj.lastName = prompt("Vvedi name");
-console.log("Hello, ", obj.name, obj.lastName);
+
+let friend = {
+  age: 30,
+  name: "Nastia",
+  lastName: "Fen'",
+  kids: 2,
+  work: false,
+};
+
+console.log("Я старше? ", me.age > friend.age);
+console.log(
+  "Длина имени моя меньше, чем у Насти ",
+  me.name.length <= friend.name.length
+);
+console.log("Детей поровну?)) ", me.kids === friend.kids);
+console.log("Ищет работу Настя? ", friend.work == false);
+console.log("Мы родственники? ", me.lastName.length === friend.lastName.length);
+
+// obj.name = prompt("Vvedi name");
+// obj.lastName = prompt("Vvedi name");
+// console.log("Hello, ", obj.name, obj.lastName);
